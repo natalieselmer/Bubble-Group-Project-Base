22873889285138e2e167b68d8a6c26e0268b78bf
Jose Esparza <73491225+JAEMANA@users.noreply.github.com>
2021-04-05 16:28:17 -0400

    changing database to ours

--- database/config.js
+++ database/config.js
@@ -2,21 +2,21 @@ export default {
   development: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
+    database: 'team_18_nba_bubble',
     host: '3.236.243.212',
     dialect: 'mysql'
   },
   test: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
+    database: 'team_18_nba_bubble',
     host: '3.236.243.212',
     dialect: 'mysql'
   },
   production: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
+    database: 'team_18_nba_bubble',
     host: '3.236.243.212',
     dialect: 'mysql'
   }
